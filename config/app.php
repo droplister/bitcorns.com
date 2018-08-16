860c714e8f587dc158b25dde13d31ebf1ae7ed73
Dan Anderson <anderson@familymedia.org>
2018-08-16 13:46:51 -0400

    Use Gstt\Achievements

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Gstt\Achievements\AchievementsServiceProvider::class,
 
         /*
          * Application Service Providers...
