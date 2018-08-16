4a2072e5ce7d164e8d16ee8c5dae2dd60b474a79
Dan Anderson <anderson@familymedia.org>
2018-08-16 13:32:14 -0400

    Models and Migrations
    That I think I need to build Bitcorn Crops on the new XCP Core.

---
+++ database/migrations/2018_08_16_172828_create_memberships_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembershipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memberships');
+    }
+}
