6fc63b8f21f33c8bef9a5d15b50ad969d6d29c3f
Dan Anderson <anderson@familymedia.org>
2018-08-16 21:49:24 -0400

    Listen to Dividends for Harvest

---
+++ app/Listeners/BitcornHarvestListener.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Listeners;
+
+use Droplister\XcpCore\App\Events\DividendWasCreated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class BitcornHarvestListener
+{
+    /**
+     * Handle the event.
+     *
+     * @param  DividendWasCreated  $event
+     * @return void
+     */
+    public function handle(DividendWasCreated $event)
+    {
+        // Check If Dividend Is Harvest
+        // Related Each Farm to Harvest
+    }
+}
