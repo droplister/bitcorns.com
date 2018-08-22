ed910af8c232e3f413830dc6333c160de4cc8c8d
Dan Anderson <anderson@familymedia.org>
2018-08-22 09:39:32 -0400

    That's not gonna work, reverting.

--- config/xcp-core.php
+++ config/xcp-core.php
@@ -22,8 +22,7 @@ return [
     /**
      * First Block
      */
-    // 'first_block' => env('XCP_CORE_ENV_NET', 'mainnet') === 'mainnet' ? 278270 : 310000,
-    'first_block' => 500000,
+    'first_block' => env('XCP_CORE_ENV_NET', 'mainnet') === 'mainnet' ? 278270 : 310000,
 
     /**
      * Sync Size (Maximum = 250)
