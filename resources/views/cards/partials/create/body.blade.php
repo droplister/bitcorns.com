a6a9c701c1dd6bfe86d555ab35ff0a1e636df81b
Dan Anderson <anderson@familymedia.org>
2018-09-18 10:22:21 -0400

    Some Copy

--- resources/views/cards/partials/create/body.blade.php
+++ resources/views/cards/partials/create/body.blade.php
@@ -1,1 +1,42 @@
-body.blade.php
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="{{ asset('images/blank-card-example.png') }}" alt="Blank Card Example" width="100%" />
+            </div>
+            <div class="col-md-8">
+                <h5 class="card-title">
+                    How it Works
+                </h5>
+                <p>Start by creating a unique Bitcorn card that you think is creative, funny, or otherwise well-suited for a blockchain farming game. Browse the <a href="{{ route('cards.index') }}">card directory</a> for inspiration and examples. Be YOU-nique!</p>
+                <p>Once you have card art, create a Counterparty asset to represent this card's supply on the blockchain.</p>
+                <p>Next, it's time to <em>burn</em>. Send whatever the current submission fee is to our burn address and 1 of your tokens to our <a href="https://bitcornmuseum.org/">Bitcorn Museum</a> burn address for permanent display.</p>
+                <p>Lastly, use the form on this page to request certification of your new cryptoart/cryptocollectible as a Bitcorn Crops Card<sup>&trade;</sup>.</p>
+                <h5 class="card-title">
+                    Requirements
+                </h5>
+                <p>1) Image must be 375 x 520 pixels.</p>
+                <p>2) Image must be PNG or GIF.</p>
+                <p>3) Issuance must be LOCKED.</p>
+                <p>4) Asset must not be divisible.</p>
+                <p>5) No subassets.</p>
+                <p>6) No NSFW content.</p>
+                <p>7) No "pre-selling" before approval.</p>
+                <h5 class="card-title">
+                    Submission Fee
+                </h5>
+                <p>Burn <s>500</s> now 1000 BITCORN to this address: 1BitcornSubmissionFeeAddressgL5Xg.</p>
+                <h5 class="card-title">
+                    Bitcorn Museum
+                </h5>
+                <p>Send 1 of your tokens here: 1BitcornCropsMuseumAddressy149ZDr.</p>
+                <h5 class="card-title">
+                    Creative Assets
+                </h5>
+                <p>Here are templates and other assets that you should use. All cards must have the "Bitcorn Border".</p>
+                <p>PSD and Font: <a href="{{ asset('assets/BitcornCardTemplatePSDfonts.zip') }}">BitcornCardTemplatePSDfonts.zip</a></p>
+                <p>Border-Only PSD: <a href="{{ asset('assets/BitcornCardTemplateBorderOnly.psd') }}">BitcornCardTemplateBorderOnly.psd</a></p>
+            </div>
+        </div>
+    </div>
+</div>
