4b16d1c3695ff9631fbf2000d0a1ae05c4ec8f5a
Dan Anderson <anderson@familymedia.org>
2018-09-18 02:21:37 -0400

    Update Card

--- resources/views/harvests/partials/index/card.blade.php
+++ resources/views/harvests/partials/index/card.blade.php
@@ -1,28 +1,21 @@
-<div class="card">
-    <div class="card-header">
-        {{ $harvest->scheduled_at->toDateString() }}
-    </div>
-    <div class="card-body">
-        <div class="row">
-            <div class="col-sm-3 col-md-2">
-                <img src="{{ $harvest->image_url }}" alt="{{ $harvest->name }}" width="100%" />
-            </div>
-            <div class="col-sm-9 col-md-10">
-                <h5 class="card-title">
-                    {{ $harvest->name }}
-                </h5>
-                <p class="card-text">
-                    {{ number_format($harvest->quantity) }} {{ config('bitcorn.reward_token') }}
-                </p>
-                <p class="card-text">
-                    {{ $harvest->content }}
-                </p>
-                @if($harvest->xcp_core_tx_index)
-                    <a href="{{ route('harvests.show', ['harvest' => $harvest->id]) }}" class="btn btn-primary">
-                        Read Almanac
-                    </a>
-                @endif
-            </div>
+<div class="card mb-4">
+    <div class="row">
+        <div class="col-sm-3 col-md-2">
+            <img src="{{ $harvest->image_url }}" alt="{{ $harvest->name }}" width="100%" />
+        </div>
+        <div class="col-sm-9 col-md-10 mt-4">
+            <h5 class="card-title">
+                {{ $harvest->name }}
+            </h5>
+            <p class="card-text mb-0">
+                {{ number_format($harvest->quantity) }} {{ config('bitcorn.reward_token') }}
+            </p>
+            <p class="card-text text-muted">
+                {{ $harvest->xcp_core_tx_index ? 'Occurred' : 'Planned' }} {{ $harvest->scheduled_at->toDateString() }}
+            </p>
+            <a href="{{ route('harvests.show', ['harvest' => $harvest->id]) }}" class="btn btn-primary">
+                Read Almanac &raquo;
+            </a>
         </div>
     </div>
 </div>
