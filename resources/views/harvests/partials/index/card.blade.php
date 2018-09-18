aeb7d8207b6faaaead8d66b3ce395e886841c8be
Dan Anderson <anderson@familymedia.org>
2018-09-18 15:36:00 -0400

    Tweak Harvest Cards

--- resources/views/harvests/partials/index/card.blade.php
+++ resources/views/harvests/partials/index/card.blade.php
@@ -5,21 +5,23 @@
                 <img src="{{ $harvest->image_url }}" alt="{{ $harvest->name }}" width="100%" />
             </a>
         </div>
-        <div class="col-sm-9 col-md-10 mt-4">
-            <h5 class="card-title">
-                <a href="{{ route('harvests.show', ['harvest' => $harvest->id]) }}" class="text-dark">
-                    {{ $harvest->name }}
+        <div class="col-sm-9">
+            <div class="harvest-body">
+                <h5 class="card-title">
+                    <a href="{{ route('harvests.show', ['harvest' => $harvest->id]) }}" class="text-dark">
+                        {{ $harvest->name }}
+                    </a>
+                </h5>
+                <p class="card-text mb-0">
+                    {{ number_format($harvest->quantity) }} {{ config('bitcorn.reward_token') }}
+                </p>
+                <p class="card-text text-muted">
+                    {{ $harvest->xcp_core_tx_index ? 'Occurred' : 'Planned' }} {{ $harvest->scheduled_at->toDateString() }}
+                </p>
+                <a href="{{ route('harvests.show', ['harvest' => $harvest->id]) }}" class="btn btn-primary">
+                    Read Almanac &raquo;
                 </a>
-            </h5>
-            <p class="card-text mb-0">
-                {{ number_format($harvest->quantity) }} {{ config('bitcorn.reward_token') }}
-            </p>
-            <p class="card-text text-muted">
-                {{ $harvest->xcp_core_tx_index ? 'Occurred' : 'Planned' }} {{ $harvest->scheduled_at->toDateString() }}
-            </p>
-            <a href="{{ route('harvests.show', ['harvest' => $harvest->id]) }}" class="btn btn-primary">
-                Read Almanac &raquo;
-            </a>
+            </div>
         </div>
     </div>
 </div>
