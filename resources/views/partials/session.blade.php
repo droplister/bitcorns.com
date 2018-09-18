0b248b9cbf5d9631cd80a164ebc715b703dabf9f
Dan Anderson <anderson@familymedia.org>
2018-09-18 14:55:47 -0400

    Update Session

--- resources/views/partials/session.blade.php
+++ resources/views/partials/session.blade.php
@@ -1,3 +1,8 @@
+@if($errors->isNotEmpty())
+    <div class="alert alert-warning mb-4" role="alert">
+        Error(s) Encountered - See warning messages below.
+    </div>
+@endif
 @if(session('error'))
     <div class="alert alert-warning mb-4" role="alert">
         {{ session('error') }}
