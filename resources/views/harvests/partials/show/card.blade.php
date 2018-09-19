367244ec57d95ac2b3a00a81a25a5a5730270acc
Dan Anderson <anderson@familymedia.org>
2018-09-19 15:51:26 -0400

    Update Table

--- resources/views/harvests/partials/show/card.blade.php
+++ resources/views/harvests/partials/show/card.blade.php
@@ -11,15 +11,15 @@
                 <table class="table mb-0">
                     <thead>
                         <tr>      
-                            <th scope="col" class="d-none d-md-inline">{{ $harvest->xcp_core_tx_index ? 'Harvested' : 'Scheduled' }}</th>
-                            <th scope="col">Bitcorn</th>
-                            <th scope="col">Farmers</th>
-                            <th scope="col">Coops</th>
+                            <th scope="col" class="d-none d-md-block border-bottom-0">{{ $harvest->xcp_core_tx_index ? 'Harvested' : 'Scheduled' }}</th>
+                            <th scope="col" class="border-bottom-0">Bitcorn</th>
+                            <th scope="col" class="border-bottom-0">Farmers</th>
+                            <th scope="col" class="border-bottom-0">Coops</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td class="d-none d-md-inline">{{ $harvest->xcp_core_tx_index ? $harvest->transaction->confirmed_at->toDateString() :  $harvest->scheduled_at->toDateString() }}</td>
+                            <td class="d-none d-md-block">{{ $harvest->xcp_core_tx_index ? $harvest->transaction->confirmed_at->toDateString() :  $harvest->scheduled_at->toDateString() }}</td>
                             <td>{{ number_format($harvest->quantity) }}</td>
                             <td>{{ number_format($harvest->farms()->count()) }}</td>
                             <td>{{ number_format($harvest->coops()->count()) }}</td>
