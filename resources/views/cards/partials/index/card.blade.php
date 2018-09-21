683bc3630fd81ac6116e516103699803a07f2932
Dan Anderson <anderson@familymedia.org>
2018-09-21 12:09:50 -0400

    Develop (#8)
    * Remove Migration Seeder from Default Seed
    
    * Harvests + Some Views
    
    * Update Harvests + Delete Tokens
    
    * Update Card
    
    * Begin Card Submission
    
    * Some Copy
    
    * Update Copy
    
    * Update Copy
    
    * Update Copy
    
    * Add Data
    
    * Division by Zero
    
    * Update Token Model
    
    * Update Cards
    
    * Update Form
    
    * Update Form
    
    * Update Form
    
    * Update Form Requests
    
    * Additional Guard
    
    * Refactor Validation
    
    * Favicon
    
    * Fix Reference
    
    * Update Session
    
    * Fix JSON
    
    * Tweak Harvest Cards
    
    * Cornculator API
    
    * Fix Reference
    
    * Try Bootstrap Vue
    
    * Add Chart
    
    * Update Calculator w/ Slider
    
    * Update
    
    * Update Modal
    
    * Improve Reactivity
    
    * Fix Typo
    
    * Add Caching
    
    * Try Watch VUEJS
    
    * Try Different Setup
    
    * Update Slider
    
    * Clear Chart Better
    
    * Update Chart
    
    * Redraw 8.0
    
    * Try Redraw AGAINNNNNNNNNNN
    
    * Update
    
    * Try Againnn
    
    * Update API
    
    * Update
    
    * Update Chart
    
    * Smaller Range
    
    * Update Chart
    
    * Update Chart
    
    * Button Color
    
    * Update Mobile View
    
    * Update Chart
    
    * Cornculator v1.0
    
    * Update Database
    
    * Update Listener
    
    * Update Naming
    
    * Update Chart
    
    * Give up on the container width issue
    
    * Harvest Show View
    
    * Update View
    
    * Update Table
    
    * Update Page
    
    * Update Table
    
    * Update Slightly
    
    * Tweak the copy
    
    * Update Copy
    
    * Redirect /almanac
    
    * Buy Page / Homepage
    
    * Update CSS
    
    * Update Content
    
    * Modify Views
    
    * Tokens Controller
    
    * Fix Reference
    
    * Update Links
    
    * Use Multiplier on Harvests for Game Effects
    
    Replace tokenBalances with balances/tokenBalances/upgradeBalances
    
    Implement Alpha Collectors, Corn Prayers, and Dry as a Bone
    
    * Fix Upgrade Tokens
    
    * Update Name Issue
    Remove Slider
    
    * Update Pricing
    
    * Cache Last Price
    
    * Calculator
    
    * Remove toFixed
    
    * Tweak Calculator
    
    Standard H1 class
    
    * Prevent Bad Input
    
    * Update Calculator
    
    * Interlinking
    
    * Tweak Padding
    
    * Moment JS
    
    * Tweak Seeder and Calculator
    
    * Alternate Columns
    
    * Update Calculator
    
    * Tweak Table
    
    * Update Column
    
    * Vue Session
    
    * Update Calculator
    
    * Fix Reference
    
    * Update Calculator
    
    * Slightly Different Setup
    
    * Whatever Ignore Bug
    
    * Update Table
    
    * Update Harvests
    
    * Change Back
    
    * Coops Mocked
    
    * Fix Reference
    
    * Fix Reference
    
    * Update This and That

--- resources/views/cards/partials/index/card.blade.php
+++ resources/views/cards/partials/index/card.blade.php
@@ -1,3 +1,6 @@
+<a href="{{ url(route('cards.show', ['token' => $card->name])) }}">
+    <img src="{{ $card->image_url }}" width="100%" alt="{{ $card->name }}" class="mb-3" />
+</a>
 <h5 class="card-title d-none d-md-block">
     <a href="{{ url(route('cards.show', ['token' => $card->name])) }}" class="text-dark">
         {{ $card->name }}
@@ -8,12 +11,6 @@
         {{ $card->name }}
     </a>
 </h6>
-<p class="card-text d-none d-md-block">
-    Harvest #{{ $card->harvest_id }} / Card #{{ $card->meta_data['overall_ranking'] }}
+<p class="card-text">
+    <span class="d-none d-md-inline">Harvest #{{ $card->harvest_id }} /</span> Card #{{ $card->meta_data['overall_ranking'] }}
 </p>
-<a href="{{ url(route('cards.show', ['token' => $card->name])) }}">
-    <img src="{{ $card->image_url }}" width="100%" alt="{{ $card->name }}" />
-</a>
-<h6 class="card-title mt-3 d-block d-md-none">
-    Card #{{ $card->meta_data['overall_ranking'] }}
-</h6>
