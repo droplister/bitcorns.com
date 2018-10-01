d8ea6adc9911c25eec1fa66c94b6c6cb87a8e945
Dan Anderson <anderson@familymedia.org>
2018-10-01 15:36:00 -0400

    Update

--- resources/views/tokens/partials/show/leaderboard.blade.php
+++ resources/views/tokens/partials/show/leaderboard.blade.php
@@ -1,4 +1,4 @@
-@if($farms->count())
+@if($balances->count())
 <div class="card mb-4">
     <div class="card-header">
         Leaderboard
@@ -6,10 +6,10 @@
     <div class="table-responsive">
         <table class="table mb-0" style="overflow-y: auto;white-space: nowrap;">
             <tbody>
-                @foreach($farms as $farm)
+                @foreach($balances as $balance)
                     <tr>
                         <th scope="row">{{ $loop->iteration }}.</th>
-                        <td><a href="{{ route('farms.show', ['farm' => $farm->slug]) }}">{{ $farm->name }}</a></td>
+                        <td><a href="{{ route('farms.show', ['farm' => $balance->farm->slug]) }}">{{ $balance->farm->name }}</a></td>
                     </tr>
                 @endforeach
             </tbody>
