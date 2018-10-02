637088f414cc6fd5f42464220310756487d4679a
Dan Anderson <anderson@familymedia.org>
2018-10-02 19:34:35 -0400

    Update Views

--- resources/views/harvests/partials/show/coop.blade.php
+++ resources/views/harvests/partials/show/coop.blade.php
@@ -1,12 +1,11 @@
 <div class="card">
-    <div class="card-header">Top Coop</div>
+    <div class="card-header">
+        Top Coop
+    </div>
     <a href="{{ route('coops.show', ['coop' => $coop->slug]) }}">
-        <img src="{{ $coop->image_url }}" alt="{{ $coop->name }}" class="w-100">
+        <img src="{{ $coop->image_url }}" alt="{{ $coop->name }}" class="w-100 border-bottom">
     </a>
     <div class="card-body">
-        <a href="{{ route('coops.show', ['coop' => $coop->slug]) }}" class="btn btn-outline-primary pull-right">
-            <i class="fa fa-map-marker"></i>
-        </a>
         <h4 class="card-title">
             <a href="{{ route('coops.show', ['coop' => $coop->slug]) }}">
                 {{ $coop->name }}
