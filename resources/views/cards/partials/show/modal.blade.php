c96da7226e1669e34567b9e9739ee52d7e211c23
Dan Anderson <anderson@familymedia.org>
2018-10-02 20:01:39 -0400

    Update Dan's Vision

--- resources/views/cards/partials/show/modal.blade.php
+++ resources/views/cards/partials/show/modal.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
+        <div class="modal-content border-0 text-center" style="background: transparent;">
             <div class="modal-body">
                 <img src="{{ $card->meta_data['hd_image_url'] }}" alt="{{ $card->name }}" width="100%" style="max-width: 750px;" />
             </div>
