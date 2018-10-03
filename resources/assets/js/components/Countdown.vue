dd536923b6081c445df4c93e357abe554c81abf2
Dan Anderson <anderson@familymedia.org>
2018-10-03 00:14:04 -0400

    Update CSS

--- resources/assets/js/components/Countdown.vue
+++ resources/assets/js/components/Countdown.vue
@@ -17,47 +17,21 @@ export default {
 
 <style>
 .vuejs-countdown {
-  margin: 6rem 0;
-  text-align: center;
-  font-family: Helvetica, Geneva, sans-serif; /* Any font would work, but SansSerif looks better */
-  text-shadow: -0.12em 0 red,  0.12em 0 cyan; /* This creates the 3D effect, pretty easy, eh? */
-  letter-spacing: 0.16em; /* make sure the letters don't overlap */
-}
-.vuejs-countdown li {
-  display: inline-block;
-  margin: 0 8px;
-  text-align: center;
-  position: relative;
-}
-.vuejs-countdown li p {
-    margin: 0;
+  margin: 6rem 0 !important;
+  text-align: center !important;
+  font-family: Helvetica, Geneva, sans-serif !important; /* Any font would work, but SansSerif looks better */
+  text-shadow: -0.12em 0 red,  0.12em 0 cyan !important; /* This creates the 3D effect, pretty easy, eh? */
+  letter-spacing: 0.16em !important; /* make sure the letters don't overlap */
 }
 .vuejs-countdown li:after {
-  content: ":";
-  position: absolute;
-  top: 0;
-  right: -27px;
-  font-size: 7em;
-}
-.vuejs-countdown li:first-of-type {
-  margin-left: 0;
-}
-.vuejs-countdown li:last-of-type {
-  margin-right: 0;
-}
-.vuejs-countdown li:last-of-type:after {
-  content: "";
+  right: -27px !important;
+  font-size: 7em !important;
 }
 .vuejs-countdown .digit {
-  font-size: 8em;
-  font-weight: 600;
-  line-height: 1.4;
-  margin-bottom: 0;
+  font-size: 8em !important;
 }
 .vuejs-countdown .text {
-  text-transform: uppercase;
-  margin-bottom: 0;
-  font-size: 1.5em;
+  font-size: 1.5em !important;
 }
 .vuejs-countdown .text:focus,
 .vuejs-countdown .digit:focus {
@@ -69,15 +43,20 @@ export default {
   line-height: 1.5em;
   transition: font-size 2s cubic-bezier(0, 1, 0, 1);
 }
-.vuejs-countdown .text:hover,
-.vuejs-countdown .digit:hover {
-  font-size: 1.5em; /* This adds a hover effect */
-  line-height: 1em;
+.vuejs-countdown .text:hover {
+  font-size: 2em; /* This adds a hover effect */
   transition: font-size .2s cubic-bezier(0, 0.75, 0, 1);
 }
-.vuejs-countdown .text:active,
+.vuejs-countdown .digit:hover {
+  font-size: 10em; /* This adds a hover effect */
+  transition: font-size .2s cubic-bezier(0, 0.75, 0, 1);
+}
+.vuejs-countdown .text:active {
+  font-size: 2em;
+  text-shadow: none; /* Click to disable the 3D effect */
+}
 .vuejs-countdown .digit:active {
-  font-size: 1em;
+  font-size: 10em;
   text-shadow: none; /* Click to disable the 3D effect */
 }
 .vuejs-countdown .text::selection,
