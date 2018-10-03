9896ad83e3fcd4bd6332f86588cfab42fc1cfc2b
Dan Anderson <anderson@familymedia.org>
2018-10-02 20:06:38 -0400

    Update

--- resources/views/cards/partials/show/modal.blade.php
+++ resources/views/cards/partials/show/modal.blade.php
@@ -2,7 +2,11 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content border-0 text-center" style="background: transparent;">
             <div class="modal-body">
-                <img src="{{ $card->meta_data['hd_image_url'] }}" alt="{{ $card->name }}" width="100%" style="max-width: 750px;" />
+                @if(isset($card->meta_data['orientation']) && $card->meta_data['orientation'] === 'portrait')
+                    <img src="{{ $card->meta_data['hd_image_url'] }}" alt="{{ $card->name }}" width="100%" style="max-width: 750px;" class="portrait" />
+                @else
+                    <img src="{{ $card->meta_data['hd_image_url'] }}" alt="{{ $card->name }}" width="100%" style="max-width: 750px;" />
+                @endif
             </div>
         </div>
     </div>
