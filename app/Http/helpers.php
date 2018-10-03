69b4cd9856f8160075b293411cd127be5ccd2448
Laravel Shift <shift@laravelshift.com>
2018-10-03 08:40:36 +0000

    Adopt PHP short array syntax
    Laravel 5 adopted the short array syntax which became available in
    PHP 5.4.

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -9,7 +9,7 @@
  */
 function ordinal($number)
 {
-    $ends = array('th','st','nd','rd','th','th','th','th','th','th');
+    $ends = ['th','st','nd','rd','th','th','th','th','th','th'];
     if ((($number % 100) >= 11) && (($number%100) <= 13)) {
         return $number. 'th';
     } else {
