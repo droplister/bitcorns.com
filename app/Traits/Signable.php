13dd2ffc04de8ffd1e65802f698e6e7008d0c84a
Dan Anderson <anderson@familymedia.org>
2018-10-03 03:41:52 -0400

    Edit Farm Again

--- app/Traits/Signable.php
+++ app/Traits/Signable.php
@@ -3,7 +3,6 @@
 namespace App\Traits;
 
 use Exception;
-use Carbon\Carbon;
 use BitWasp\BitcoinLib\BitcoinLib;
 use App\Http\Requests\Farms\UpdateRequest;
 
@@ -16,44 +15,10 @@ trait Signable
      */
     public function validateSignature(UpdateRequest $request)
     {
-        // Validate Timestamp
-        if($this->guardAgainstInvalidTimestamp($request))
-        {
-            return 'Invalid Timestamp';
-        }
-
         // Validate Signature
         if($this->guardAgainstInvalidSignature($request))
         {
             return 'Invalid Signature';
-        }
-
-        // No issues
-        return false;
-    }
-
-    /**
-     * Guard Against Invalid Timestamp
-     * 
-     * @param  \App\Http\Requests\Farms\UpdateRequest  $request
-     * @return boolean
-     */
-    private function guardAgainstInvalidTimestamp(UpdateRequest $request)
-    {
-        try
-        {
-            // Carbon
-            $timestamp = Carbon::parse($request->timestamp);
-
-            // 1-hour (or less)
-            if($timestamp < Carbon::now()->subHour())
-            {
-                return true; // Too old
-            }
-        }
-        catch(Exception $e)
-        {
-            return true; // Invalid
         }
 
         // No issues
@@ -73,7 +38,7 @@ trait Signable
             // Bitcoin Lib
             $messageVerification = BitcoinLib::verifyMessage(
                 $this->xcp_core_address,
-                $request->signature,
+                $request->message,
                 $request->timestamp
             );
 
---
+++ resources/views/farms/partials/edit/head.blade.php
@@ -0,0 +1,5 @@
+<section class="jumbotron text-center" style="background: url({{ $farm->display_image_url }}) no-repeat center center / cover;">
+    <a href="{{ url(route(farms.show', ['farm' => $farm->slug])) }}" class="btn btn-sm btn-light">
+        <i class="fa fa-eye"></i> View
+    </a>
+</section>
