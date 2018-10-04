2e6cae323ac9879fbb349d678a57443c9d5059e2
Dan Anderson <anderson@familymedia.org>
2018-10-04 06:21:59 -0400

    Update Telegram

--- resources/views/home/partials/head.blade.php
+++ resources/views/home/partials/head.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ config('bitcorn.telegram') }}" class="btn btn-outline-primary mt-3 float-right" target="_blank">
-    <i class="fa fa-telegram"></i> Telegram
+    <i class="fa fa-telegram"></i> <span class="d-none d-sm-inline">Telegram</span>
 </a>
 <h1 class="display-4 my-5 text-uppercase">
     Bitcorn <span class="d-none d-sm-inline">Crops</span>
