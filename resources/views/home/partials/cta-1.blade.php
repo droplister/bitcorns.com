f70e3416367aca8dd88d6f685bd827d52fef329e
Dan Anderson <anderson@familymedia.org>
2021-09-21 01:19:03 -0400

    Update cta-1.blade.php

--- resources/views/home/partials/cta-1.blade.php
+++ resources/views/home/partials/cta-1.blade.php
@@ -1,8 +1,8 @@
 <div class="alert alert-success my-5 text-center">
-    <strong>News Alert:</strong>
+    <strong>New Here?</strong>
     <strong>
-        <a href="https://medium.com/@BitcornCrops/bitcorn-cards-added-to-book-of-orbs-d96374403d27" target="_blank">
-            BITCORN Added to BoO!
+        <a href="https://t.me/bitcorns" target="_blank">
+            Join our Telegram!
         </a>
     </strong>
 </div>
