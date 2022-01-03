42cd98e9208b25c66f1c41d02e0fe15422eb4147
philippnagel <phil@philippnagel.com>
2022-01-03 10:40:29 +0100

    Update card.blade.php

--- resources/views/farms/partials/show/card.blade.php
+++ resources/views/farms/partials/show/card.blade.php
@@ -1,5 +1,7 @@
 @if($card->name === 'CORNFETTI' && $balance->quantity_normalized >= config('bitcorn.min_cornfetti'))
     <cornfetti></cornfetti>
+@elseif($card->name === 'CORNTONE' && $balance->quantity_normalized >= config('bitcorn.min_corntone'))
+    <corntone></corntone>  
 @elseif($card->name === 'FULLCORN')
     <img src="{{ $card->image_url }}" width="100%" alt="{{ $card->name }}" class="mb-3" id="fullcorn_button" style="cursor: pointer;" />
 @else
